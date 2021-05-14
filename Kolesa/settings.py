175b76e753fa867b8c3dadbf19d08222c907fe5e
pr1nce07 <58590243+pr1nce07@users.noreply.github.com>
2021-05-15 05:19:49 +0600

    @qwe

--- Kolesa/settings.py
+++ Kolesa/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'Kolesa.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "Kolesa",
+        'NAME': "qwe",
         'USER': "postgres",
         'PASSWORD': "12345",
         'HOST': 'localhost',
