3b00cb0f021682b9278dd93a9da5ba9ad224bcfa
pr1nce07 <58590243+pr1nce07@users.noreply.github.com>
2021-05-15 04:54:52 +0600

    az

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
-    path('one/', views.One.as_view())
+    path('one/', views.One.as_view()),
+    path('cars/', views.CarViewSet.as_view({'get': 'list'})),
+    path('cars/add/', views.CarViewSet.as_view({'post': 'create'})),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,32 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+
+
+
 
 
 class One(APIView):
     def get(self):
         pass
+
+
+@permission_classes([AllowAny])
+
+class CarViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        queryset = Car.objects.all()
+        serializer = CarSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+    def create(self, request):
+        serializer = CarSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
