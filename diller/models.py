ba30e814f70fb2bb0ec227d2840cf74140be1a2d
beketx <beketsk@gmail.com>
2021-05-15 03:51:56 +0600

    one

--- diller/models.py
+++ diller/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from auth_.models import User
+from main.models import Car, City
+
+
+class Favourites(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+
+
+class Publications(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.IntegerField()
+    year = models.IntegerField()
--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+
+
+class City(models.Model):
+    name = models.CharField(max_length=150)
+
