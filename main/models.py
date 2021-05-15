e4cc83dea6c2b5f485abe1452c5b3cf0c824ce52
pr1nce07 <58590243+pr1nce07@users.noreply.github.com>
2021-05-15 06:17:10 +0600

    dasf

--- main/models.py
+++ main/models.py
@@ -4,21 +4,14 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 
-class AbstractModel(models.Model):
-    name = models.CharField(max_length=150)
-    description = models.TextField(blank=True)
-
-    class Meta:
-        abstract = True
-
 
 class CityManager(models.Manager):
 
     def order_by_name(self):
         return self.filter().order_by('name')
 
-class City(AbstractModel):
-
+class City(models.Model):
+    name = models.CharField(max_length=150)
 
     objects = CityManager()
 
@@ -30,23 +23,18 @@ class CarManager(models.Manager):
     def order_by_name(self):
         return self.filter().order_by('name')
 
-class Car(AbstractModel):
-
+class Car(City):
+    description = models.TextField(blank=True)
 
     objects = CarManager()
-    def __str__(self):
-        return self.name
 
-class Category(AbstractModel):
+class Category(City):
+    description = models.TextField(blank=True)
 
-<<<<<<< HEAD
+
 class SubCategory(models.Model):
     car_id = models.ForeignKey(Car, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=120)
-=======
-    def __str__(self):
-        return self.name
->>>>>>> c0c99cb96e54c2efe888f20922620d98b643636a
 
     def __str__(self):
         return self.name
