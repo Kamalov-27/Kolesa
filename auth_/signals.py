6a12df4bf567ee491a4954114a4529b335b29a56
beketx <beketsk@gmail.com>
2021-05-15 08:19:42 +0600

    one

--- auth_/signals.py
+++ auth_/signals.py
@@ -7,6 +7,7 @@ from . import models
 @receiver(post_save, sender=models.User)
 def developer_created(sender, instance, created, **kwargs):
     if created:
+
         models.Profile.objects.create(
             first_name='Ashr',
             last_name='Ashe',
@@ -15,7 +16,5 @@ def developer_created(sender, instance, created, **kwargs):
             gender='M'
         )
 
-@receiver(post_delete, sender=models.Favorites)
-def favorite_bool(sender, instance, **kwargs):
-    if instance.favorite_bool == False:
-        Favourites.objects.get(user=instance.user).delete()
+# @receiver(post_delete, sender=models.Favorites)
+# def favorite_bool(sender, instance, **kwargs):
--- main/models.py
+++ main/models.py
@@ -1,6 +1,38 @@
-from django.db import models
+# from django.db import models
+#
+# # Create your models here.
+# from django.db import models
+# from django.contrib.auth.models import User
+#
+#
+# class CityManager(models.Manager):
+#
+#     def order_by_name(self):
+#         return self.filter().order_by('name')
+#
+# class City(models.Model):
+#     name = models.CharField(max_length=150)
+#
+#     objects = CityManager()
+#
+#     def __str__(self):
+#         return self.name
+#
+# class CarManager(models.Manager):
+#
+#     def order_by_name(self):
+#         return self.filter().order_by('name')
+#
+# class Car(City):
+#     description = models.TextField(blank=True)
+#
+#     objects = CarManager()
+#
+# class Category(City):
+#     description = models.TextField(blank=True)
+#
+#
 
-# Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -42,3 +74,20 @@ class Category(AbstractModel):
     def __str__(self):
         return self.name
 
+
+class SubCategory(models.Model):
+    car_id = models.ForeignKey(Car, on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
+class EngineType(models.Model):
+    name = models.CharField(max_length=120)
+    is_electric = models.BooleanField(default=False)
+
+
+class KP(models.Model):
+    name = models.CharField(max_length=120)
+    is_full_drive = models.BooleanField(default=False)
